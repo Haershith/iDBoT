c41003372aa06820609e1e2e0b778a1bd9f86b3b
Mutyala Harshith <105526704+Mega-Hero@users.noreply.github.com>
2022-06-26 12:37:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,26 +11,25 @@ Bot = Client(
 )
 
 START_TEXT = """<b>Hello {}
-I am a Simple Telegram Info Gathering Bot, Click /help to know my Commands and my uses @MutyalaHarshith<b>"""
+I am a Simple Telegram Info Bot, Click /help for more information<b>"""
 
-HELP_TEXT = """🤔 How to use me @MutyalaHarshith?
-• Forward a Message for take it's Details (in Private)
-• Send any Media to take its Details (in private)
-• Reply /info to a Message to take Message Details
-• Use /info Command to take your Details
-• Use /id in Group or Channel to get Unique Telegram ID"""
+HELP_TEXT = """💞 How to Use Me Harshith
+• Send Me a file and reply by /mhinfo
+• Send any Media to take its Details
+• Reply /mhinfo to a Message to take Message Details
+• Use /mhinfo Command to take your Details
+• Use /mhid in Group or Channel to get Unique Telegram ID"""
 
-ABOUT_TEXT = """--**About You From @MutyalaHarshith**--
+ABOUT_TEXT = """--**About You From MHiNFoBoT**--
 - **Bot :** `Info Bot`
 - **Creator :** [Harshith](https://t.me/MutyalaHarshith)
-- **Credits :** `Everyone in this journey`
-- **Join :** [Telugu RoBoTs](https://t.me/Telugu_RoBoTs)
+- **Deploy OwN :** [Tutorial]()
+- **Join :** [MHGcHaT](https://t.me/MHGcHaT)
 - **Language :** [Python3](https://python.org)
 - **Library :** [Pyrogram v1.2.0](https://pyrogram.org)
-- **Server :** [Heroku](https://heroku.com)"""
+- **Server :** [Okteto](https://heroku.com)"""
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton(text="Join", url=f"https://t.me/MutyalaHarshith")]])
-
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton(text="💞 Join", url=f"https://t.me/MutyalaHarshith")]])
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start(bot, update):
@@ -59,15 +58,15 @@ async def about(bot, update):
     )
 
 
-@Bot.on_message(filters.private & filters.command("info"))
+@Bot.on_message(filters.private & filters.command("mhinfo"))
 async def info(bot, update):
     
-    text = f"""--**Information from @MutyalaHarshith**--
-** 📛 First Name :** {update.from_user.first_name}
-**📛 Your Second Name :** {update.from_user.last_name if update.from_user.last_name else 'None'}
-**👾 Your Username :** {update.from_user.username}
-**🆔 Your Telegram ID :** {update.from_user.id}
-**🔗 Your Profile Link :** {update.from_user.mention}"""
+    text = f"""--**Information from Harshith**--
+**💞 First Name :** {update.from_user.first_name}
+**😎 Your Second Name :** {update.from_user.last_name if update.from_user.last_name else 'None'}
+**🥳 Your Username :** {update.from_user.username}
+**😜 Your Telegram ID :** {update.from_user.id}
+**🤫 Your Profile Link :** {update.from_user.mention}"""
     
     await update.reply_text(        
         text=text,
@@ -76,10 +75,10 @@ async def info(bot, update):
     )
 
 
-@Bot.on_message(filters.private & filters.command("id"))
+@Bot.on_message(filters.private & filters.command("mhid"))
 async def id(bot, update):
     await update.reply_text(        
-        text=f"**Your Telegram ID :** {update.from_user.id}",
+        text=f"💞 **Your Telegram ID :** {update.from_user.id}",
         disable_web_page_preview=True,
         reply_markup=BUTTONS
     )
