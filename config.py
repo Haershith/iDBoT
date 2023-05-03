f1f1140864f843cfa7669e0a59030935cf6307af
Mutyala Harshith <105526704+Haershith@users.noreply.github.com>
2023-05-03 07:31:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import re
 
 from os import environ
 
-API_ID = int(environ.get('API_ID', ''))
+API_ID = int(environ.get('API_ID', '22842195'))
 
-API_HASH = environ.get('API_HASH', '')
+API_HASH = environ.get('API_HASH', '92e2b15a1227cae25efe7a8b0197ef9c')
 
 BOT_TOKEN = environ.get('BOT_TOKEN', '5314659103:AAHIMXet_4lEliD6SoFetL7bh4uzSSzL5ko')
