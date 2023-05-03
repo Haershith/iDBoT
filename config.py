3aadae52bb1c60f58c7f9f1a560454f1804a20ef
Mutyala Harshith <105526704+Haershith@users.noreply.github.com>
2023-05-03 07:26:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import re
+
+from os import environ
+
+API_ID = int(environ.get('API_ID', ''))
+
+API_HASH = environ.get('API_HASH', '')
+
+BOT_TOKEN = environ.get('BOT_TOKEN', '5314659103:AAHIMXet_4lEliD6SoFetL7bh4uzSSzL5ko')
